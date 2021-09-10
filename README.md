b35bcdbea881fc2391ba82c84a38a7f87a6950a8
Pavel <pzlacfa@gmail.com>
2021-09-10 16:12:50 +0300

    added new readme and yaml configuration

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-A simple project for that get information from blockchain and proceed it ML Saprk job
+A simple project that get information from blockchain and proceed it to MLSaprk job
 
 Pipeline:
-GraphQL -> Kafka -> Spark structured streaming -> ML job -> HDFS
+GraphQL -> Kafka -> Spark structured streaming -> MLSpark job -> HDFS
+
+# How to Install:
+   + pip3 install pyyaml
+   + pip3 install kafka-python
+
 
 
 ## Blockchain info
--- graphqlReq.py
+++ graphqlReq.py
@@ -2,10 +2,20 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
+import yaml
+from kafka import KafkaProducer
 
+
+# Reading configuration file
+# ======================================================================================================
+with open("project.yaml", "r") as stream:
+    try:
+        configuration = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
 
 def run_query(query):  # A simple function to use requests.post to make the API call.
-    headers = {'X-API-KEY': 'TODO'}
+    headers = {'X-API-KEY': configuration['BITQUERY_API_KEY']}
     request = requests.post('https://graphql.bitquery.io/',
                             json={'query': query}, headers=headers)
     if request.status_code == 200:
@@ -14,8 +24,8 @@ def run_query(query):  # A simple function to use requests.post to make the API 
         raise Exception('Query failed and return code is {}.      {}'.format(request.status_code,
                         query))
 
-
 # The GraphQL query
+# ======================================================================================================
 query = """
 query{
   bitcoin{
@@ -25,13 +35,17 @@ query{
    }
 }
 """
-
+# =======================================================================================================
 
 
 result = run_query(query)  # Execute the query
 print(f"Result - {result}")
-
 mjson = json.loads(f"{result}".replace("\'", "\""))
-print(type(mjson))
-#print(mjson['data']['bitcoin']['blocks']['count'])
 print(mjson['data']['bitcoin']['blocks'][0]['count'])
+
+
+# Send message to Kafka producer
+# ======================================================================================================
+producer = KafkaProducer(bootstrap_servers=[configuration['KAFKA_BOOTSTRAP']],
+                         value_serializer=lambda x: 
+                         json.dumps(x).encode('utf-8'))
